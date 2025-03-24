1b0c7962d9a43287a211e4f16ecda975c01a1cb6
HOELCODE <hcostantini1@gmail.com>
2025-03-24 21:56:56 +0100

    revu fichier router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,7 @@ import Shaping from "../layout/Shaping";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Card from "../pages/Card";
+import ErrorPage from "../pages/ErrorPage";
 
 const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/logement/:id" element={<Card />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   );
